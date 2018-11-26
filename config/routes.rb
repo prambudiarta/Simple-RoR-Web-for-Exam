d01b980cd554ed5ad6d2e62b58e395f3536bca99
gitgalih <prambudiarta@gmail.com>
2018-11-26 14:25:15 +0700

    first init

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  get 'sessions/new'
+  get 'users/new'
+  get '/home', to: 'page#home'
+  get '/contact', to: 'page#contact'
+  get '/help', to: 'page#help'
+  get '/about', to: 'page#about'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+ root 'page#home'
+ resources :users
+  end
